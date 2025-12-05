eda823f24db074e5e9f948ad0d39fdf4e3d13ead
V COIN Admin <admin@3dvcoin.com>
2025-12-05 13:12:33 +0900

    Remove webpack config for Turbopack compatibility

--- next.config.ts
+++ next.config.ts
@@ -4,18 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  // firebase-admin은 서버 사이드에서만 사용
-  serverExternalPackages: ['firebase-admin'],
-  experimental: {
-    serverComponentsExternalPackages: ['firebase-admin'],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = config.externals || [];
-      config.externals.push('firebase-admin');
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
