3f95ba84fc9a4248f6823e2dbba8f2f633144847
V COIN Admin <admin@3dvcoin.com>
2025-12-05 12:36:47 +0900

    Fix firebase-admin module not found - add webpack externals

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,16 @@ const nextConfig: NextConfig = {
   },
   // firebase-admin은 서버 사이드에서만 사용
   serverExternalPackages: ['firebase-admin'],
+  experimental: {
+    serverComponentsExternalPackages: ['firebase-admin'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push('firebase-admin');
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
